f802d46cb1313c6de244b2a2fac2c370cae54985
root <root@DESKTOP-6O3M852.localdomain>
2021-06-29 22:03:41 -0300

    arrow functions

---
+++ js-expert/arrow-functions.js
@@ -0,0 +1,17 @@
+// funtions 
+
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(2,2));
+
+// Arrow function
+
+const sum = (num1, num2) => num1 + num2;
+
+console.log(sum(3,3));
+
+const sayHello = (name) => `Hello ${name}`;
+
+console.log(sayHello("Banzak"));
