74d04b70e97394ca5fe42578010492aef6ed8a49
banzak1 <leosantanabr@gmail.com>
2021-07-01 14:03:48 -0300

    ciração do database

---
+++ JavascriptII-Project/database.js
@@ -0,0 +1,11 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Digital Miimalism",
+        autor: "Cal Newpart",
+        categoria: "Produtividade e estilo de vida",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+]
