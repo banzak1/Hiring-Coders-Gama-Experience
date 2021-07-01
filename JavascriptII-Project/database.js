5813d128ec98a0823d65aada3920799f897144ee
banzak1 <leosantanabr@gmail.com>
2021-07-01 15:05:12 -0300

    criação das regras de negocio

--- JavascriptII-Project/database.js
+++ JavascriptII-Project/database.js
@@ -3,9 +3,47 @@ const livros = [
         id: 1,
         nome: "Digital Miimalism",
         autor: "Cal Newpart",
-        categoria: "Produtividade e estilo de vida",
+        categoria: "Produtividade",
         paginas: 254,
         recomenda: false,
         leu: false
     },
+    {
+        id: 2,
+        nome: "PHP",
+        autor: "Cal Newpart",
+        categoria: "Programação",
+        paginas: 347,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 3,
+        nome: "Eloquente Javascript",
+        autor: "Cal Newpart",
+        categoria: "Programação",
+        paginas: 685,
+        recomenda: true,
+        leu: false
+    },
+    {
+        id: 4,
+        nome: "HTML & CSS",
+        autor: "Cal Newpart",
+        categoria: "Programação",
+        paginas: 457,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: "Pequeno Principe",
+        autor: "Cal Newpart",
+        categoria: "Literatura",
+        paginas: 198,
+        recomenda: true,
+        leu: false
+    }
 ]
+
+module.exports = livros;
