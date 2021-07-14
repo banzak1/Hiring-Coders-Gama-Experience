09e5fbbbbc29c57c19fbe9b4cc2c4b01c955f17d
Banzak-Uninter <leosantanabr@gmail.com>
2021-07-14 16:30:20 -0300

    estilização utilizando styled-component

---
+++ modulo-react/gamagit/src/Pages/Repositories/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as S from './styled';
+
+export default function Repositories() {
+    return (
+        <>
+        <S.title>Repositório</S.title>
+        <S.list>
+            <S.listItem>Repositório: Nome Repositório</S.listItem>  
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+            <S.listItem>Repositório: Nome Repositório</S.listItem>        
+        </S.list>
+        </>
+    );
+};
---
+++ modulo-react/gamagit/src/Pages/Repositories/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const title = styled.h1`
+    text-align:center;
+    font-size: 2rem;
+    font-family: sans-serif;
+    color: #333;
+`
+
+export const list = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+`
+
+export const listItem = styled.li`
+    margin: 0.5rem 0;
+    background: #000;
+    color: #fff;
+    padding: 0.5rem;
+`
